73f29040e04be7757a6fd14e97e385c74d02a809
Mucyo_chris <chrisxeventy6@gmail.com>
2025-05-06 19:42:08 +0200

    Update LoginController.php

--- LoginController.php
+++ LoginController.php
@@ -9,33 +9,11 @@ use Illuminate\Support\Facades\Lang;
 
 class LoginController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
 
     use AuthenticatesUsers{
         sendFailedLoginResponse as superSendFailedLoginResponse;
     }
-
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
     protected $redirectTo = '/';
-
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
         $this->middleware('guest', ['except' => 'logout']);
